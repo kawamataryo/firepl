b4c4aef884e270e2588ae9e9208d656b1af027e6
kawamataryou <ba068082@gmail.com>
2021-09-11 09:52:41 +0900

    fix: add welcom message

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,6 @@ import { getArgs } from "./lib/args";
 const args = getArgs();
 const firepl = new Firepl(args.credentials);
 
+console.log("Welcome to Firepl.\nexit using Ctrl+D or type .exit\n");
 const customRepl = repl.start({ prompt: "🔥> ", eval: asyncEval });
 customRepl.context.firepl = firepl;
